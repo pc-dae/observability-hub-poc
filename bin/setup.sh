2c06796d4eaceb5f22553ae466ef6cba238be0aa
Paul Carlton <paul.carlton@dae.mn>
2025-11-12 19:19:58 +0000

    monitoring deployment

--- bin/setup.sh
+++ bin/setup.sh
@@ -218,4 +218,4 @@ secrets.sh $debug_str --tls-skip --secrets $PWD/resources/secrets/github-secrets
 kubectl rollout restart deployment -n external-secrets external-secrets
 
 kubectl apply -f local-cluster/addons.yaml
-kubectl apply -f local-cluster/addons-appset.yaml
+kubectl apply -f local-cluster/grafana-appset.yaml
